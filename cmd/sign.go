bf344bf2c5b353c2f881c24c059d5435ee833297
Roy Ginting <roy@xendit.co>
2023-01-01 16:28:08 +0700

    Add flag to upload signature file to S3 bucket

--- cmd/sign.go
+++ cmd/sign.go
@@ -24,6 +24,7 @@ import (
 
 var signingKey string
 var moduleSignatureSuffix string
+var uploadSignature bool
 
 // signCmd represents the sign command
 var signCmd = &cobra.Command{
@@ -32,13 +33,16 @@ var signCmd = &cobra.Command{
 	Example: `  terraform-module-cosign sign --key <key path>|<kms uri> <module archive>
 	
   # sign Terraform module archive using local private key
-  terraform-module-cosign sign --key private.key example-module.zip
+  terraform-module-cosign sign --key cosign.key example-module.zip
 	
   # sign Terraform module archive using key from AWS KMS
   terraform-module-cosign sign --key awskms://[ENDPOINT]/[ID/ALIAS/ARN] example-module.zip
   
    # sign Terraform module archive on S3 bucket
-   terraform-module-cosign sign --key awskms://[ENDPOINT]/[ID/ALIAS/ARN] s3::https://example-bucket.s3.ap-southeast-1.amazonaws.com/example-module.zip`,
+   terraform-module-cosign sign --key cosign.key s3::https://example-bucket.s3.ap-southeast-1.amazonaws.com/example-module.zip
+   
+   # sign Terraform module archive on S3 bucket and upload the signature file to the same bucket
+   terraform-module-cosign sign --key cosign.key --upload-signature s3::https://example-bucket.s3.ap-southeast-1.amazonaws.com/example-module.zip`,
 	Args: cobra.ArbitraryArgs,
 	Run: func(cmd *cobra.Command, args []string) {
 		ro := &options.RootOptions{
@@ -68,7 +72,7 @@ var signCmd = &cobra.Command{
 		}
 
 		for _, arg := range args {
-			var module string
+			var module, region, bucket, key string
 
 			if strings.HasPrefix(arg, "s3::") {
 				// Download module archive from S3 bucket to temporary directory
@@ -76,10 +80,10 @@ var signCmd = &cobra.Command{
 
 				u, err := url.Parse(s3ObjectUrl)
 				if err != nil {
-					log.Fatalf("Failed to parse url [%s]: %s", s3ObjectUrl, err.Error())
+					log.Fatalf("Failed to parse url [%s]: %v", s3ObjectUrl, err)
 				}
 
-				region, bucket, key, _, err := parseUrl(u)
+				region, bucket, key, _, err = parseUrl(u)
 				if err != nil {
 					log.Fatal(err.Error())
 				}
@@ -88,19 +92,22 @@ var signCmd = &cobra.Command{
 
 				s3Downloader, err := newS3Downloader(region)
 				if err != nil {
-					log.Fatalf("Failed to create S3 downloader: %s", err.Error())
+					log.Fatalf("Failed to create S3 downloader: %v", err)
 				}
 
 				moduleFile, err := os.Create(module)
 				if err != nil {
-					log.Fatalf("Failed to create archive on local filesystem: %s", err.Error())
+					log.Fatalf("Failed to create archive on local filesystem: %v", err)
 				}
+				defer os.Remove(module)
 
-				s3Downloader.Download(moduleFile, &s3.GetObjectInput{
+				_, err = s3Downloader.Download(moduleFile, &s3.GetObjectInput{
 					Bucket: aws.String(bucket),
 					Key:    aws.String(key),
 				})
-
+				if err != nil {
+					log.Fatalf("Failed to download module archive from S3: %v", err)
+				}
 			} else {
 				module = arg
 			}
@@ -108,7 +115,31 @@ var signCmd = &cobra.Command{
 			// Sign module archive
 			moduleSignature := fmt.Sprintf("%s%s", module, moduleSignatureSuffix)
 			if _, err := sign.SignBlobCmd(ro, ko, module, true, moduleSignature, "", false); err != nil {
-				log.Fatalf("Error signing [%s]: %s", module, err.Error())
+				log.Fatalf("Error signing [%s]: %v", module, err)
+			}
+
+			if uploadSignature && strings.HasPrefix(arg, "s3::") {
+				signatureKey := strings.Replace(key, module, moduleSignature, 1)
+
+				s3Uploader, err := newS3Uploader(region)
+				if err != nil {
+					log.Fatalf("Failed to create S3 uploader: %v", err)
+				}
+
+				signatureFile, err := os.Open(moduleSignature)
+				if err != nil {
+					log.Fatalf("Failed to open signature file: %v", err)
+				}
+				defer os.Remove(moduleSignature)
+
+				_, err = s3Uploader.Upload(&s3manager.UploadInput{
+					Bucket: aws.String(bucket),
+					Key:    aws.String(signatureKey),
+					Body:   signatureFile,
+				})
+				if err != nil {
+					log.Fatalf("Failed to upload signature file to S3: %v", err)
+				}
 			}
 		}
 	},
@@ -119,6 +150,7 @@ func init() {
 	signCmd.Flags().StringVar(&signingKey, "key", "", "path to the private key file or KMS URI")
 	signCmd.MarkFlagRequired("key")
 	signCmd.Flags().StringVar(&moduleSignatureSuffix, "suffix", ".sig", "suffix for module archive signature")
+	signCmd.Flags().BoolVar(&uploadSignature, "upload-signature", false, "whether to upload signature file to remote location")
 }
 
 func newS3Downloader(region string) (*s3manager.Downloader, error) {
@@ -128,6 +160,15 @@ func newS3Downloader(region string) (*s3manager.Downloader, error) {
 
 	downloader := s3manager.NewDownloader(sess)
 	return downloader, nil
+}
+
+func newS3Uploader(region string) (*s3manager.Uploader, error) {
+	sess := session.Must(session.NewSessionWithOptions(session.Options{
+		Config: aws.Config{Region: aws.String(region)},
+	}))
+
+	Uploader := s3manager.NewUploader(sess)
+	return Uploader, nil
 }
 
 func parseUrl(u *url.URL) (region, bucket, path, version string, err error) {
