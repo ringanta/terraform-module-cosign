4a5c6867f49f6950b1d4ca53aabc9677e8310633
Roy Ginting <roy@xendit.co>
2023-01-02 11:56:20 +0700

    Add version

--- cmd/root.go
+++ cmd/root.go
@@ -20,6 +20,7 @@ It leverages cosign to create and verify signature of module archive.`,
 // Execute adds all child commands to the root command and sets flags appropriately.
 // This is called by main.main(). It only needs to happen once to the rootCmd.
 func Execute() {
+	rootCmd.Version = "0.1.0"
 	err := rootCmd.Execute()
 	if err != nil {
 		os.Exit(1)
